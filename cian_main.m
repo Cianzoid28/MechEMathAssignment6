380064355412988ef3d64130404e3bb334bd7476
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-12-07 22:13:37 -0500

    Adding Nat_freq sim and string params func.

--- cian_main.m
+++ cian_main.m
@@ -1,16 +1,17 @@
 close all;clc; clear;
-
 num_masses = 4;
 total_mass = 1;
 tension_force = 10;   % N
 string_length = 10;  % m
+amplitude_Uf = 0.01;
+
+
 damping_coeff = string_length / tension_force;
 dx = string_length / (num_masses + 1);
 
 string_params = struct();
 string_params.n = num_masses;
 string_params.M = total_mass;
-
 string_params.Tf = tension_force;
 string_params.L = string_length;
 string_params.c = damping_coeff;
@@ -24,7 +25,6 @@ nat_freqs = sqrt(diag(lambda_mat));
 string_params.Ur = Ur_mat;
 string_params.nat_freqs = nat_freqs;
 
-amplitude_Uf = 0.01;
 omega_Uf = nat_freqs(1);
 
 % boundary forcing at right end
@@ -32,5 +32,4 @@ Uf_func    = @(t) amplitude_Uf * cos(omega_Uf * t);
 dUfdt_func = @(t) -omega_Uf * amplitude_Uf * sin(omega_Uf * t);
 string_params.Uf_func = Uf_func;
 string_params.dUfdt_func = dUfdt_func;
-
-string_simulation(string_params)
+nat_freq_string_simulation(string_params)
